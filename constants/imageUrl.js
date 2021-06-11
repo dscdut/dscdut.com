9a353348619da5e2eeae921cf1e81a00ada0c61e
Tan Phat <nguyentanphat30@gmail.com>
2021-06-11 13:21:37 +0700

    modify imageUrl.js

--- constants/imageUrl.js
+++ constants/imageUrl.js
@@ -1,5 +1,5 @@
 const ImageUrl = {
-  IMAGE_BIG_URL: 'https://res.cloudinary.com/dz5pvwzm5/image/upload/c_scale,h_900,w_600/v1618733095/dsc_images/%20new',
+  IMAGE_BIG_URL: 'https://res.cloudinary.com/dz5pvwzm5/image/upload/c_scale,h_600,w_400/v1618733095/dsc_images/%20new',
   IMAGE_MEDIUM_URL: 'https://res.cloudinary.com/dz5pvwzm5/image/upload/c_fill,g_face,h_300,q_100,w_300/v1620809320/dsc_images/%20new',
   IMAGE_THUMBNAIL_URL: 'https://res.cloudinary.com/dz5pvwzm5/image/upload/c_scale,h_180,w_120/v1618733095/dsc_images/%20new',
   IMAGE_AVATAR_URL: 'https://res.cloudinary.com/dz5pvwzm5/image/upload/c_scale,h_90,w_60/v1618733095/dsc_images/%20new/',
