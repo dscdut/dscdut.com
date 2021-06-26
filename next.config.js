4bbb44d12f6b9ed94b097adf74d51b1261ffa612
prd-y-nguyen <y.nguyen@paradox.ai>
2021-06-26 11:31:04 +0700

    :rocket: Deploy to netlify

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@
 // const { i18n } = require('./next-i18next.config');
 
 module.exports = {
+  // Target must be serverless
+  target: 'serverless',
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
